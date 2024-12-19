52284a1cd2439d0ff2f2a539037609fae9d79168
DroppedHard <szyper7@gmail.com>
2024-12-19 02:58:25 +0100

    update cors

--- cmd/middleware/cors.go
+++ cmd/middleware/cors.go
@@ -5,7 +5,7 @@ import "net/http"
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Allow all origins (temporarily)
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://pogodynka-app.netlify.app/, https://weather-forecast-app-j7gf.onrender.com")
 		// Allow specific HTTP methods
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		// Allow specific headers
