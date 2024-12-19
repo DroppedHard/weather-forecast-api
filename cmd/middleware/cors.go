8ae59b47882f024792195473534329d559024ca7
DroppedHard <szyper7@gmail.com>
2024-12-19 03:45:35 +0100

    CORS update

--- cmd/middleware/cors.go
+++ cmd/middleware/cors.go
@@ -1,12 +1,35 @@
 package middleware
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func EnableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		allowedOrigins := []string{
+			"https://pogodynka-app.netlify.app",
+			"https://weather-forecast-app-j7gf.onrender.com",
+			"http://localhost:4173",
+		}
+		log.Printf("Incoming request from Origin: %s\n", r.Header.Get("Origin"))
+		origin := r.Header.Get("Origin")
+		isAllowed := false
+		for _, allowedOrigin := range allowedOrigins {
+			if origin == allowedOrigin {
+				isAllowed = true
+				break
+			}
+		}
+
+		if isAllowed {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Set("Access-Control-Allow-Origin", "https://pogodynka-app.netlify.app/, https://weather-forecast-app-j7gf.onrender.com, http://localhost:4173")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Allow-Headers")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
